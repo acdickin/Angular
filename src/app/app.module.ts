fc33f264e21f04eb3aa356038c8289647c35c5c6
Andrew Dickinson <andrew.dickinson@t2.local>
2016-11-01 15:41:58 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component'
@@ -13,7 +14,11 @@ import { TodosComponent } from './todos/todos.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:"name",component: },
+      {path:"name",component: }
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
